2af04876ab35973c984437d36fe7efcfe505203e
jfbm74 <jfbm74@gmail.com>
2021-04-15 15:38:59 -0500

    Adding task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""""Get the transpose of a matrix using numpy """
+
+
+def np_transpose(matrix):
+    """Get the transpose of a matrix using numpy """
+    import numpy as np
+    return np.transpose(matrix)

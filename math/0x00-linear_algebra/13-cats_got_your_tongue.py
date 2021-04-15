d5a80fbf653de851ca5f8c2d0e53daa9a1ed5aee
jfbm74 <jfbm74@gmail.com>
2021-04-15 15:47:53 -0500

    concatenates 2 matrices with given axis

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Concatenates two matrices along a specific axis"""
+"""Concatenates two matrices along a given axis"""
 
 
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """Concatenates two matrices along a specific axis"""
+    """Concatenates two matrices along a given axis"""
     return np.concatenate((mat1, mat2), axis=axis)

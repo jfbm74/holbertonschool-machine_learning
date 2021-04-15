ae5a636ff3ce0d930a94efaca8165f1da543a405
jfbm74 <jfbm74@gmail.com>
2021-04-15 15:51:07 -0500

    Multimiplation with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" implements matrix multiplication  with numpy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ implements matrix multiplication  with numpy """
+    return np.dot(mat1, mat2)

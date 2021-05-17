d66c8eddd1aa64d344049d5e418d0a867bcaf554
jfbm74 <jfbm74@gmail.com>
2021-05-17 13:53:07 -0500

    Single neuron

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Defines a single neuron performing binary classification
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """
+        Class constructor
+        nx is the number of input features to the neuron
+        """
+        # Validations
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        # weights vector for the neuron
+        # default mean is 0
+        # default stddev is 1
+        self.nx = nx
+        self.W = np.random.randn(1, nx)
+        # bias for the neuron
+        self.b = 0
+        # activated output of the neuron (prediction)
+        self.A = 0

f42b2e64fd10896f8ad3bb829e7691cc2ee1726b
jfbm74 <jfbm74@gmail.com>
2021-05-17 15:20:28 -0500

    Evaluates the neuron’s predictions

--- supervised_learning/0x01-classification/4-neuron.py
+++ supervised_learning/0x01-classification/4-neuron.py
@@ -59,5 +59,5 @@ class Neuron:
     def evaluate(self, X, Y):
         """ Evaluates the neuron’s predictions """
         A = self.forward_prop(X)
-        a = np.where(A < 0.5, 0, 1)
+        a = np.where(A >= 0.5, 1, 0)
         return A, self.cost(Y, A)

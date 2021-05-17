8861d03e64fb388d2d74e6ac0f0fdd6fc349aeb1
jfbm74 <jfbm74@gmail.com>
2021-05-17 14:05:54 -0500

    Private Neuron

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -3,12 +3,12 @@
 Defines a single neuron performing binary classification
 """
 
-
 import numpy as np
 
 
 class Neuron:
     """Defines a single neuron performing binary classification"""
+
     def __init__(self, nx):
         """
         Class constructor
@@ -20,8 +20,20 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
-        self.W = np.random.randn(1, nx)
+        self.__W = np.random.randn(1, nx)
         # bias for the neuron
-        self.b = 0
+        self.__b = 0
         # activated output of the neuron (prediction)
-        self.A = 0
+        self.__A = 0
+
+        @property
+        def W(self):
+            return self.__W
+
+        @property
+        def b(self):
+            return self.__b
+
+        @property
+        def A(self):
+            return self.__A

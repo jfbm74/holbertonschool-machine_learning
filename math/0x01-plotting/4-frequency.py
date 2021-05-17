48d5627a62a5e602d74bdeef882857d257cddfbc
jfbm74 <jfbm74@gmail.com>
2021-05-16 20:59:15 -0500

    code to plot a histogram of student scores for a project

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# your code here
+
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.xticks(range(0, 101, 10))
+plt.title("Project A")
+plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.show()

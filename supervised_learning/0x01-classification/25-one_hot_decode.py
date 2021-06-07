a8a9a6c26956ea22ca56fe2f37899192fd34a0e2
jfbm74 <jfbm74@gmail.com>
2021-06-07 16:50:56 -0500

    coverts one hot matriz in vector of labels

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+"""
+Converts a one-hot matrix into a vector of labels
+"""
+
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """     Converts a one-hot matrix into a vector of labels"""
+
+    if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
+        return None
+
+    return(np.argmax(one_hot, axis=0))

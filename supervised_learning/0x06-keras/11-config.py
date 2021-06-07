e0d1841d9ca984b28e2d0c76fd45343efdeb1e1f
jfbm74 <jfbm74@gmail.com>
2021-06-06 20:26:20 -0500

    saves a model’s configuration in JSON format

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-"""Module that saves and loads a model’s configuration"""
+"""saves a model’s configuration in JSON format"""
 import tensorflow.keras as K
 
 
 def save_config(network, filename):
     """saves a model’s configuration in JSON format
-    Args:
-        network: is the model whose configuration should be saved
-        filename: path of the file that the configuration should be saved to
-    Returns: None
     """
     with open(filename, 'w') as f:
         f.write(network.to_json())
@@ -16,10 +12,6 @@ def save_config(network, filename):
 
 def load_config(filename):
     """loads a model with a specific configuration
-    Args:
-        filename is the path of the file containing
-            the model’s configuration in JSON format
-    Returns: the loaded model
     """
     with open(filename, 'r') as f:
         config = f.read()

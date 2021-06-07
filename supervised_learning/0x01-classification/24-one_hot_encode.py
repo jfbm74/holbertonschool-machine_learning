8f7df1c586c411f228c106c85765ec0d0333cf55
jfbm74 <jfbm74@gmail.com>
2021-06-07 16:48:11 -0500

    one hot encode

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Module that converts a numeric label vector into a one-hot matrix"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """ Function that converts a numeric label vector into a one-hot matrix
+    """
+    if type(classes) is not int or classes <= 0:
+        return None
+    if type(Y) is not np.ndarray:
+        return None
+
+    m = Y.shape[0]
+    try:
+        one_hot = np.zeros((classes, m))
+        for i in range(m):
+            row = Y[i]
+            one_hot[row][i] = 1
+        return one_hot
+    except Exception:
+        return None

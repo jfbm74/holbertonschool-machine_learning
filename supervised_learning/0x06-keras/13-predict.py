0583ee08c1b457670f6783ff7dc9046ca56192c2
jfbm74 <jfbm74@gmail.com>
2021-06-06 20:28:33 -0500

    Function that makes a prediction using a neural network

--- supervised_learning/0x06-keras/13-predict.py
+++ supervised_learning/0x06-keras/13-predict.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-"""Modulethat makes a prediction using a neural network"""
+"""Module that makes a prediction using a neural network"""
 import tensorflow.keras as K
 
 
 def predict(network, data, verbose=False):
-    """makes a prediction using a neural network
-    Args:
-        network is the network model to make the prediction with
-        data: is the input data to make the prediction with
-        verbose: is a boolean that determines if output
-            should be printed during the prediction process
-    Returns: the prediction for the data
+    """Function that makes a prediction using a neural network
     """
     return network.predict(data, verbose=verbose)

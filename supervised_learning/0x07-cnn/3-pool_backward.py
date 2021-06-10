d4d996b94656981279b8cd5d3ef234a7ed7467a9
jfbm74 <jfbm74@gmail.com>
2021-06-10 11:54:53 -0500

    propagation over a pooling

---
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module that performs back propagation over a pooling
+ layer of a neural network"""
+
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """Function that performs back propagation over a pooling
+    layer of a neural network"""
+    dA_prev = np.zeros(A_prev.shape)
+    for m in range(dA.shape[0]):
+        for i in range(dA.shape[1]):
+            x = i * stride[0]
+            for j in range(dA.shape[2]):
+                y = j * stride[1]
+                for k in range(dA.shape[3]):
+                    if mode == 'max':
+                        A_min = A_prev[m, x:x + kernel_shape[0],
+                                       y:y + kernel_shape[1], k]
+                        slope = (A_min == np.max(A_min))
+                        dA_prev[m, x:x + kernel_shape[0],
+                                y:y + kernel_shape[1],
+                                k] += slope*dA[m, i, j, k]
+                    else:
+                        dAmin = dA[m, i, j, k]/kernel_shape[0]/kernel_shape[1]
+                        dA_prev[m, x:x + kernel_shape[0],
+                                y:y + kernel_shape[1], k] += dAmin
+    return dA_prev

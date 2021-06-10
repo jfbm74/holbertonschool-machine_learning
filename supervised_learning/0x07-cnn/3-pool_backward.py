783bf412b5f691af85017bf666a975cb25988275
jfbm74 <jfbm74@gmail.com>
2021-06-10 11:59:12 -0500

    propagation over a pooling

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python3
-"""Module that performs back propagation over a pooling
- layer of a neural network"""
+"""
+Module Performs back propagation over a pooling layer of a neural network
+"""
+
 
 import numpy as np
 
 
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """Function that performs back propagation over a pooling
-    layer of a neural network"""
+    """
+    Function that performs back propagation over a pooling layer of a
+     neural network
+    """
+    m = dA.shape[0]
+    h_new = dA.shape[1]
+    w_new = dA.shape[2]
+    c = dA.shape[3]
+    kh = kernel_shape[0]
+    kw = kernel_shape[1]
+    sh = stride[0]
+    sw = stride[1]
     dA_prev = np.zeros(A_prev.shape)
-    for m in range(dA.shape[0]):
-        for i in range(dA.shape[1]):
-            x = i * stride[0]
-            for j in range(dA.shape[2]):
-                y = j * stride[1]
-                for k in range(dA.shape[3]):
+    for i in range(m):
+        for cn in range(c):
+            for h in range(h_new):
+                for w in range(w_new):
                     if mode == 'max':
-                        A_min = A_prev[m, x:x + kernel_shape[0],
-                                       y:y + kernel_shape[1], k]
-                        slope = (A_min == np.max(A_min))
-                        dA_prev[m, x:x + kernel_shape[0],
-                                y:y + kernel_shape[1],
-                                k] += slope*dA[m, i, j, k]
-                    else:
-                        dAmin = dA[m, i, j, k]/kernel_shape[0]/kernel_shape[1]
-                        dA_prev[m, x:x + kernel_shape[0],
-                                y:y + kernel_shape[1], k] += dAmin
+                        aux = A_prev[i,
+                                     h * sh: h * sh + kh,
+                                     w * sw: w * sw + kw,
+                                     cn]
+                        mask = aux == np.max(aux)
+                        dA_prev[i,
+                                h * sh: h * sh + kh,
+                                w * sw: w * sw + kw,
+                                cn] += dA[i, h, w, cn] * mask
+                    if mode == 'avg':
+                        dA_prev[i,
+                                h * sh: h * sh + kh,
+                                w * sw: w * sw + kw,
+                                cn] += dA[i, h, w, cn] / (kh * kw)
     return dA_prev

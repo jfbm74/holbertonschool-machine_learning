7d9f1d7012b6156b938f4a85df9577074ed00edd
jfbm74 <jfbm74@gmail.com>
2021-09-19 18:07:18 -0500

    Fixing task 100 adv

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -1,55 +1,53 @@
 #!/usr/bin/env python3
-"""Continuous bayesian inference"""
+"""
+Calculates the posterior probability that the probability of
+developing severe side effects falls within a specific range given the data"""
 
 
 from scipy import special
-import numpy as np
 
 
 def posterior(x, n, p1, p2):
-    """ calculates posterior probability for the various hypothetical
-            probabilities of developing severe side effects given the data
-        x # of patients that develop severe side effects
-        n is the total number of patients observed
-        p1 is the lower bound on the range
-        p2 is the upper bound on the range
-        You can assume the prior beliefs of p follow a uniform distribution
-        Returns: 1D ndarray containing the intersection of obtaining
-            x and n with each probability in P, respectively
+    """
+    Returns: the posterior probability that
+    p is within the range [p1, p2] given x and n
     """
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
+
     if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer that is greater " +
-                         "than or equal to 0")
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(p1, float) or\
-            not (np.all(p1 >= 0) and np.all(p1 <= 1)):
-        raise TypeError("p1 must be a float in the range [0, 1]")
-    if not isinstance(p2, float) or\
-            not (np.all(p2 >= 0) and np.all(p2 <= 1)):
-        raise TypeError("p1 must be a float in the range [0, 1]")
+
+    if type(p1) is not float or p1 < 0 or p1 > 1:
+        raise ValueError("p1 must be a float in the range [0, 1]")
+
+    if type(p2) is not float or p2 < 0 or p2 > 1:
+        raise ValueError("p2 must be a float in the range [0, 1]")
+
     if p2 <= p1:
         raise ValueError("p2 must be greater than p1")
 
-    def factorial(m):
-        """ calculates factorial of n """
-        return np.math.factorial(m)
-    #p = np.uniform.random(range(1, 11))
-    #print("p", p)
-    beta = special.beta(p1, p2)
-    print("beta", beta)
-    P = np.linspace(0, 1, 11)
-    print("P", P)
-    likelihood = np.ndarray(P.shape)
-    for p in range(len(P)):
-        fact_n = factorial(n)
-        pA = (factorial(n) /
-              (factorial(x) * factorial(n - x)))
-        pB = (np.power(P[p], x)) *\
-             (np.power((1 - P[p]), (n - x)))
-        likelihood[p] = pA * pB
-    intersection = likelihood * beta
-    marginal = np.sum(intersection)
-    return (intersection / (marginal))
+    """
+    Cumulative distribution function of the beta distribution.
+    Returns the integral from zero to u
+    of the beta probability density function.
+    btdtr(a, b, u)
+    a: Shape parameter (a > 0)
+    b: Shape parameter (b > 0)
+    u: Upper limit of integration, in [0, 1]
+    """
+    # x follows a binomial distribution
+    # Relation between beta distribution and binomial distribution
+    a = x + 1
+    b = n - x + 1
+
+    cdf_beta1 = special.btdtr(a, b, p1)
+    cdf_beta2 = special.btdtr(a, b, p2)
+
+    Posterior = cdf_beta2 - cdf_beta1
+
+    return Posterior

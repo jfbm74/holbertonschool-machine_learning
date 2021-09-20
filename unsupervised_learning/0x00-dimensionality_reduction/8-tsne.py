23ab95bae566df514507e991b6d93ade0d91a414
jfbm74 <jfbm74@gmail.com>
2021-09-19 20:07:00 -0500

    task 8

--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -13,36 +13,30 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     """
     T-sne function
     """
-    n, d = X.shape
-    initial_momentum = 0.5
-    final_momentum = 0.8
-
-
     X = pca(X, idims)
-    P = P_affinities(X, perplexity=perplexity)
+    n, d = X.shape
+    P = P_affinities(X, perplexity=perplexity) * 4
     Y = np.random.randn(n, ndims)
-    iY = Y
-
-    P = P * 4.
+    iY = np.zeros((n, ndims))
 
     for i in range(iterations):
-
         dY, Q = grads(Y, P)
+
         if i <= 20:
-            momentum = initial_momentum
+            momentum = 0.5
         else:
-            momentum = final_momentum
+            momentum = 0.8
 
         iY = momentum * iY - lr * dY
-        Y = Y + iY
-        Y = Y - np.tile(np.mean(Y, 0), (n, 1))
+        Y = Y + iY - np.tile(np.mean(Y, 0), (n, 1))
 
-        if (i + 1) % 100 == 0:
+        if (i + 1) != 0 and (i + 1) % 100 == 0:
             C = cost(P, Q)
-            print('Cost at iteration {}: {}'.format((i+1), C))
+            print("Cost at iteration {}: {}".format(i + 1, C))
 
-        if i == 100:
+        if (i + 1) == 100:
             P = P / 4.
 
     return Y
+
 
